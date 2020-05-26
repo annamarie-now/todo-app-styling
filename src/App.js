23639fdfcf0102826489d72a7f87a7d21fc87465
Rune Høck Rapley-Møller <runehm@me.com>
2020-05-26 15:35:35 +0200

    adds css loader in webpack and simple styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './styles.css';
+
 
 const App = () => {
 
-	const todos = [
+	const todosInitState = [
 		{
 			title: 'item 1'
 		},
@@ -14,24 +16,62 @@ const App = () => {
 		}
 	];
 
+	const [todos, setTodos] = useState(todosInitState);
+	const [value, setValue] = useState('');
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (!value) return;
+		handleAddTodo(value);
+		setValue('');
+	};
+
+	const handleAddTodo = (text) => {
+		const newTodos = [...todos, {title: text}];
+		setTodos(newTodos);
+
+	};
+
+	const handleDelete = (title) => {
+		const newTodos = todos.filter(todo => todo.title != title);
+		setTodos(newTodos);
+	};
+
 	return (
-		<div>
+		<div className='wrapper'>
 			<h1>Todoooooooo app</h1>
 
 			<h3>Was ich machen werde wenn die kontaktsperre aufgehoben wird:</h3>
 
 			<ul>
 				{
-					todos.map((todo, index) => {
-						return (
-							<li key={index}>
-								{todo.title}
-							</li>
-						);
-					})
+					todos.length > 1 ?
+						todos.map(todo => {
+							return (
+								<li key={todo.title}>
+									{todo.title}
+									<button onClick={() => {
+										handleDelete(todo.title);
+									}}>Delete
+									</button>
+								</li>
+							);
+						}) : null
 				}
 			</ul>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					className='input'
+					value={value}
+					placeholder='Add Todo...'
+					onChange={e => setValue(e.target.value)}
+				/>
+				<button type='submit'>Add</button>
+			</form>
 		</div>
 	);
 };
 export default App;
+
